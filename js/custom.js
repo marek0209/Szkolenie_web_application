e9b609c433b669af52709e5834f10ac8bb220cd8
Marek Urbaniec <marek.urbaniec1@gmail.com>
2018-08-20 10:43:55 +0200

    v7

--- js/custom.js
+++ js/custom.js
@@ -1,39 +1,46 @@
 var czas_opoznienia;
-function delay(){
-		if ( $(document).ready()){
-		czas_opoznienia = setTimeout(showPage, 2000);
+function delay() {
+	czas_opoznienia = setTimeout(showPage, 3000);
+	
+	
+	// dodano automatyczna wysokosc sekcji 1
+	window_size = $(window).height();
+	$('section.one').css("height",window_size);
+}
 
-		windows_size = $(window).height();
-		$('section.one').css("height", windows_size);
-		}
-	}
 function showPage(){
 	document.getElementById('preloader').style.display = "none";
 	document.getElementById('body').style.display = "block";
-	window.dispatchEvent(new Event('resize'));
-}
-window.onload = delay;
 
-//przylepiony header
+	// dodano zdarzenia resize
+    window.dispatchEvent(new Event('resize'));
+} 
 
-var header = document.getElementById('header');
-var position_header_before = 30;
 
-window.onscroll = function(){
-	sticky_head();
-}
-function sticky_head(){
-	if (window.pageYOffset >= position_header_before){
-		header.classList.add('fixed-top');
-		
+
+	var header = document.getElementById('header');
+	var position_header_before = 30;
+
+	// przylepione header
+	window.onscroll = function(){
+		sticky_head();
 	}
-		else{
+
+	function sticky_head(){
+		if (window.pageYOffset >= position_header_before) {
+			header.classList.add('fixed-top');
+		}else{
 			header.classList.remove('fixed-top');
+		}
 	}
-}
 
-var element_listy = $ ('section#tabs > div >.tabs_container > #pills-tab > li.nav-item');
-element_listy.click(function(){
-	$('#pills-tab > li').removeClass('active_tab');
+var elementy_listy = $('section#tabs > div >.tabs_container > #pills-tab > li.nav-item');
+
+elementy_listy.click(function(){
+
+	elementy_listy.removeClass('active_tab');
 	$(this).addClass('active_tab');
-})
+
+});
+
+
